345773d79f8b8b89e0d4fb5caeab957b7afbcc48
Jaby <fenir50@web.de>
2021-01-03 15:38:31 +0100

    Created first overlay

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#define _WCHAR_T
 #include "PlanschbeckenOverlay/Planschbecken.h"
+#include <stddef.h>
+#include <libcd.h>
 #include <stdio.h>
+
+extern "C" u_long __load_start_ovly0;
 
 int main()
 {
-    return printf(Planschbecken::text);
+	CdInit();
+	
+	CdReadFile("\\PLANSCH.bin;1", &__load_start_ovly0, 0);
+	CdReadSync(0, nullptr);
+
+	return printf(Planschbecken::text);
 }
